5cf0bf5d997473e797018792301a259442c88ece
Wenchu Du <wduu0003@student.monash.edu>
2019-03-23 17:01:40 +1100

    Delete comment in User class

--- src/main/java/rockets/model/User.java
+++ src/main/java/rockets/model/User.java
@@ -3,7 +3,7 @@ package rockets.model;
 import java.util.Objects;
 
 import static org.apache.commons.lang3.Validate.notBlank;
-//LLLLLLL
+
 public class User extends Entity {
     private String firstName;
 
